e1d1ab85f850de4b720b81ebbff28874731f02d8
Jean Nunes <jeanclaudionunessantosnunes@gmail.com>
2024-02-19 05:01:54 -0300

    feat(CONFIG): config | implements logger in main and update packages

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	config "application-openings/config"
 	router "application-openings/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorWithValues("Config init error: %v", err)
+		return
+	}
+
 	router.InitRouter()
 }
