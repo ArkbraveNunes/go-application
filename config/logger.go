2df36c1e53b79e69cad75104e0d87cfa02e0da2a
Jean Nunes <jeanclaudionunessantosnunes@gmail.com>
2024-02-19 04:59:02 -0300

    feat(CONFIG): config | create config subpackage and config file and logger file

---
+++ config/logger.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+	writer  io.Writer
+}
+
+func NewLogger(prefix string) *Logger {
+	writer := io.Writer(os.Stdout)
+	logger := log.New(writer, prefix, log.Ldate|log.Ltime)
+
+	return &Logger{
+		debug:   log.New(writer, "DEBUG: ", logger.Flags()),
+		info:    log.New(writer, "INFO: ", logger.Flags()),
+		warning: log.New(writer, "WARNING: ", logger.Flags()),
+		err:     log.New(writer, "ERROR: ", logger.Flags()),
+		writer:  writer,
+	}
+}
+
+func (logger *Logger) Debug(v ...interface{}) {
+	logger.debug.Print(v...)
+}
+func (logger *Logger) DebugWithValues(format string, v ...interface{}) {
+	logger.debug.Printf(format, v...)
+}
+
+func (logger *Logger) Info(v ...interface{}) {
+	logger.info.Print(v...)
+}
+func (logger *Logger) InfoWithValues(format string, v ...interface{}) {
+	logger.info.Printf(format, v...)
+}
+
+func (logger *Logger) Warn(v ...interface{}) {
+	logger.warning.Print(v...)
+}
+func (logger *Logger) WarnWithValues(format string, v ...interface{}) {
+	logger.warning.Printf(format, v...)
+}
+
+func (logger *Logger) Error(v ...interface{}) {
+	logger.err.Print(v...)
+}
+func (logger *Logger) ErrorWithValues(format string, v ...interface{}) {
+	logger.err.Printf(format, v...)
+}
