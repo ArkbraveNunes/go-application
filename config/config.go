c8ee6272b2967c685dc9da7f5adee010356b2a62
Jean Nunes <jeanclaudionunessantosnunes@gmail.com>
2024-02-19 10:11:37 -0300

    feat(CONFIG): config | create mysql config file

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
-	gorm "gorm.io/gorm"
+	"fmt"
+
+	"gorm.io/gorm"
 )
 
 var (
@@ -10,7 +12,18 @@ var (
 )
 
 func Init() error {
+	var err error
+
+	db, err = InitMySQL()
+	if err != nil {
+		return fmt.Errorf("Init Mysql database error: %v", err)
+	}
+
 	return nil
+}
+
+func GetMySql() *gorm.DB {
+	return db
 }
 
 func GetLogger(prefix string) *Logger {
---
+++ config/mysql.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"application-openings/schema"
+
+	mysql "gorm.io/driver/mysql"
+	gorm "gorm.io/gorm"
+)
+
+func InitMySQL() (*gorm.DB, error) {
+	logger := GetLogger("mySQL")
+
+	db, err := gorm.Open(mysql.Open("local:local@tcp(localhost:3306)/application-openings"), &gorm.Config{})
+
+	if err != nil {
+		logger.ErrorWithValues("mysql database opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schema.Opening{})
+	if err != nil {
+		logger.ErrorWithValues("mysql auto migrate error: %v", err)
+	}
+
+	return db, nil
+}
