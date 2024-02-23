a6350d0a2ba0a4440b658414d1e4a5b96ca3d576
Jean Nunes <jeanclaudionunessantosnunes@gmail.com>
2024-02-23 06:45:58 -0300

    feat(ROUTER): router | update routes file

--- router/routes.go
+++ router/routes.go
@@ -4,11 +4,20 @@ import (
 	"application-openings/handler"
 
 	"github.com/gin-gonic/gin"
+
+	docs "application-openings/docs"
+
+	docsFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initRoutes(router *gin.Engine) {
 	handler.InitHandler()
-	routerGroupV1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+
+	docs.SwaggerInfo.BasePath = basePath
+
+	routerGroupV1 := router.Group(basePath)
 
 	routePrefixOpening := "/opening"
 	{
@@ -18,5 +27,7 @@ func initRoutes(router *gin.Engine) {
 		routerGroupV1.PUT(routePrefixOpening, handler.UpdateOpeningHandler)
 		routerGroupV1.DELETE(routePrefixOpening, handler.DeleteOpeningHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(docsFiles.Handler))
 
 }
